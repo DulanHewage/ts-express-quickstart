6ab644af0557c9637e9ccbeeeb1001d803d1d841
Dulan Hewage <dulanhewage2@gmail.com>
2024-01-20 02:06:21 +0530

    add dist to gitignore, update nodemon settings

--- dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-// Define your routes here
-router.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
-export default router;
-//# sourceMappingURL=index.js.map
